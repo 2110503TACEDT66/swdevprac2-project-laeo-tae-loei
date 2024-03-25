523041249a69c13c1a22297ca1f462054e783bb6
martnarabodee <martnarabodee@gmail.com>
2024-03-25 14:54:25 +0700

    + auth & interface

---
+++ interface.ts
@@ -0,0 +1,26 @@
+interface HotelItem {
+    _id: string,
+    name: string,
+    address: string,
+    telephoneNumber: string,
+    starRating: number,
+    basePrice: number,
+    tel: string,
+    images: { [key: string]: string }
+}
+  
+interface HotelJSON {
+  success: boolean,
+  count: number,
+  pagination: Object,
+  data: HotelItem[]
+}
+
+interface BookingItem {
+  user: string; 
+  hotel: string; 
+  roomType: string;
+  duration: number;
+  bookDate: Date;
+  createdAt: Date;
+}
