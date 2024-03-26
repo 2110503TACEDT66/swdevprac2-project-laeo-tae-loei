dfd4d7c52350cd839e63616519641a521e610823
martnarabodee <martnarabodee@gmail.com>
2024-03-27 00:37:41 +0700

    add booking/manage for redirecting after auth

---
+++ src/app/booking/manage/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/');
+    }, []);
+
+    return (
+        <div className='w-screen h-screen text-xl font-bold text-center'>
+            Redirecting...
+        </div>
+    );
+}
